6c8e13be1f8f1a5f9cc2b0d944dab85fe7d94c4e
Arthur Arakelyan <artur.araqelyan.2004@gmail.com>
2026-05-06 01:59:59 +0300

    Added wallpaper alchemy most popular wallpapers background

--- src/plugins/shared/WallpaperAlchemy/helpers/tag/index.ts
+++ src/plugins/shared/WallpaperAlchemy/helpers/tag/index.ts
@@ -1,7 +1,7 @@
-import { storageUrl } from "@/constants/main";
-import { lowerCaseImageExtension } from "@/helpers/image";
-import { checkString } from "@/helpers/string";
-import { ITag, ITagLocalized } from "@/types";
+import { storageUrl } from "../../constants/main";
+import { ITag, ITagLocalized } from "../../types";
+import { lowerCaseImageExtension } from "../image";
+import { checkString } from "../string";
 
 export const getTagUrl = (tag: ITag | ITagLocalized): string => {
   return `/${tag.name}/wallpapers`;
@@ -25,13 +25,4 @@ export const getTagImageHeight = (
   tag: ITag | ITagLocalized,
 ): number | undefined => {
   return tag.height || undefined;
-};
-
-export const getTagGtagData = (tag: ITag | ITagLocalized) => {
-  return {
-    id: tag.id,
-    name: tag.name,
-    title: tag.title,
-    image: getTagImage(tag) || "",
-  };
 };
