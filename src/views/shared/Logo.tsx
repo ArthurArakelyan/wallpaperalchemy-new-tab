5acc47573519c369364dfa3e7a0dfb3696fea596
Arthur Arakelyan <artur.araqelyan.2004@gmail.com>
2026-05-10 15:26:13 +0300

    Added wallpaper alchemy logo

--- src/views/shared/Logo.tsx
+++ src/views/shared/Logo.tsx
@@ -1,7 +1,39 @@
 import "./Logo.sass";
 
 import type { FC } from "react";
+import { useIntl } from "react-intl";
 
-const Logo: FC = () => <h1 className="Logo">Wallpaper Alchemy</h1>;
+import { siteName } from "../../plugins/shared/WallpaperAlchemy/constants/main";
+import {
+  getLocalizedAbsoluteUrl,
+  intlLocaleToWallpaperAlchemyLocale,
+} from "../../plugins/shared/WallpaperAlchemy/utilities/i18n";
+
+const Logo: FC = () => {
+  const intl = useIntl();
+
+  return (
+    <a
+      className="logo"
+      href={getLocalizedAbsoluteUrl(
+        "/",
+        intlLocaleToWallpaperAlchemyLocale(intl.locale),
+      )}
+      target="_blank"
+    >
+      <img
+        draggable={false}
+        src="/icons/128.png"
+        alt={siteName}
+        width={32}
+        height={32}
+        loading="lazy"
+        className="logoImage"
+      />
+
+      <h1 className="logoTitle">{siteName}</h1>
+    </a>
+  );
+};
 
 export default Logo;
